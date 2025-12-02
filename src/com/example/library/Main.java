57c4f367e9773b011255646145b6338f806987f5
Jodie Weldon <weldon.jodie@gmail.com>
2025-12-02 09:58:02 -0800

    Add book editor

--- src/com/example/library/Main.java
+++ src/com/example/library/Main.java
@@ -1,29 +1,23 @@
 package com.example.library;
 
-import com.example.library.forms.BookEditorExample;
-import com.example.library.forms.SaveButtonListener;
+import com.example.library.forms.BookEditor;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         // Launch the book editor form
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                BookEditorExample bookEditorExample = new BookEditorExample();
-                bookEditorExample.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            BookEditor bookEditor = new BookEditor();
+            bookEditor.setVisible(true);
 
-                bookEditorExample.setSaveButtonListener(new SaveButtonListener() {
-                    @Override
-                    public void onSaveClicked(Book book) {
-                        System.out.println("Entered Book Details:");
-                        System.out.println("Book Title: " + book.getName());
-                        System.out.println("Author: " + book.getAuthor().getName());
-                        System.out.println("Genre: " + book.getGenre());
-                        System.out.println("Is Unavailable: " + book.isTaken());
-                    }
-                });
-            }
+            bookEditor.setSaveButtonListener(book -> {
+                System.out.println("Entered Book Details:");
+                System.out.println("Book Title: " + book.getName());
+                System.out.println("Author: " + book.getAuthor().getName());
+                System.out.println("Genre: " + book.getGenre());
+                System.out.println("Is Unavailable: " + book.isTaken());
+            });
         });
     }
 }
--- src/com/example/library/forms/BookEditor.java
+++ src/com/example/library/forms/BookEditor.java
@@ -3,98 +3,72 @@ package com.example.library.forms;
 import com.example.library.*;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class BookEditorExample extends JFrame{
+public class BookEditor extends JFrame{
     private JPanel contentPane;
+    private JTextField bookNameField;
     private JTextField authorNameField;
     private JComboBox<Genre> genreComboBox;
-    private JTextField bookNameField;
     private JCheckBox isTakenCheckBox;
     private JButton saveButton;
     private JButton cancelButton;
 
-    private Library library;
     private SaveButtonListener saveButtonListener;
 
-    public BookEditorExample() {
-        library = new Library();
-
+    public BookEditor()  {
         setTitle("Book Editor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(contentPane);
         pack();
 
-        // Populate the genre combo box
         populateGenreComboBox();
 
-        // Set the frame location to the center of the screen
         setLocationRelativeTo(null);
 
-        // Save button event listener
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveChanges();
-            }
-        });
+        saveButton.addActionListener(e -> saveChanges());
 
-        // Cancel button event listener
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cancelChanges();
-            }
-        });
+        cancelButton.addActionListener(e -> cancelChanges());
 
-        // Set the frame visible
         setVisible(true);
     }
-    public void setSaveButtonListener(SaveButtonListener listener) {
-        this.saveButtonListener = listener;
+
+    private void cancelChanges() {
+        bookNameField.setText("");
+        authorNameField.setText("");
+        genreComboBox.setSelectedIndex(0);
+        isTakenCheckBox.setSelected(false);
     }
+
     private void saveChanges() {
         String authorName = authorNameField.getText();
         String bookName = bookNameField.getText();
         Genre genre = (Genre) genreComboBox.getSelectedItem();
         boolean isTaken = isTakenCheckBox.isSelected();
 
-        // Create Author object
-        Author author = new Author(authorName, ""); // Set the author name
+        Author author = new Author(authorName, "");
 
-        // Create Book object
         Book book = new Book(author, genre, null, bookName);
         book.setTaken(isTaken);
 
-        // Notify the listener with the book object
         if (saveButtonListener != null) {
             saveButtonListener.onSaveClicked(book);
         }
 
-        // Reset fields
         authorNameField.setText("");
         bookNameField.setText("");
         genreComboBox.setSelectedIndex(0);
         isTakenCheckBox.setSelected(false);
     }
 
-
-    private void cancelChanges() {
-        // Reset fields
-        bookNameField.setText("");
-        genreComboBox.setSelectedIndex(0);
-        isTakenCheckBox.setSelected(false);
-    }
-
     private void populateGenreComboBox() {
-        // Get the combo box model
         DefaultComboBoxModel<Genre> comboBoxModel = (DefaultComboBoxModel<Genre>) genreComboBox.getModel();
 
-        // Add genre values to the combo box model
         for (Genre genre : Genre.values()) {
             comboBoxModel.addElement(genre);
         }
     }
 
+    public void setSaveButtonListener(SaveButtonListener listener) {
+        this.saveButtonListener = listener;
+    }
 }
